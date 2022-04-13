fc6c2aaf9f6b45171da614d6a2e0339012ddc910
Vlados <vladnaumkin2005@yandex.ru>
2022-04-13 18:34:10 +0300

    text

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from data import db_session
 import json
 from forms.user import RegisterForm, LoginForm, ChangeForm
 from forms.news import NewsForm
+from forms.question import QuestionForm
+from forms.quiz import QuizForm
 from data.users import User
 from data.news import News
 from werkzeug.utils import secure_filename
@@ -18,7 +20,6 @@ app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static', 'img')
 db_session.global_init("db/database.db")
 login_manager = LoginManager()
 login_manager.init_app(app)
-
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)
@@ -27,6 +28,7 @@ patch_request_class(app)
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
+    a = 1
     return db_sess.query(User).get(user_id)
 
 
@@ -113,6 +115,69 @@ def logout():
     return redirect("/")
 
 
+@app.route('/addquestion', methods=['GET', 'POST'])
+def addquestion():
+    form = QuestionForm()
+    if form.validate_on_submit():
+        if form.head.data == '':
+            return render_template('makequestions.html',
+                                   form=form,
+                                   message="Пустой заголовок")
+        if form.tema.data == '':
+            return render_template('makequestions.html',
+                                   form=form,
+                                   message="Нет темы")
+        if form.photo.data == '':
+            return render_template('makequestions.html',
+                                   form=form,
+                                   message="Нет ФОТО")
+        db_sess = db_session.create_session()
+        filename = photos.save(form.photo.data)
+        file_url = photos.url(filename)
+        quest = QuestionForm(
+            title=form.head.data,
+            content=form.text.data,
+            tema=form.tema.data,
+            img_news='static/img/' + filename,
+        )
+        db_sess.add(quest)
+        db_sess.commit()
+        return redirect('/quizzes')
+    return render_template('makequestions.html', title='Регистрация', form=form)
+
+
+@app.route('/addquiz', methods=['GET', 'POST'])
+def addquiz():
+    form = QuizForm()
+    if form.validate_on_submit():
+        if form.head.data == '':
+            return render_template('makequizzes.html',
+                                   form=form,
+                                   message="Пустой заголовок")
+        if form.tema.data == '':
+            return render_template('makequizzes.html',
+                                   form=form,
+                                   message="Нет темы")
+        if form.photo.data == '':
+            return render_template('makequizzes.html',
+                                   form=form,
+                                   message="Нет ФОТО")
+        db_sess = db_session.create_session()
+        filename = photos.save(form.photo.data)
+        file_url = photos.url(filename)
+        quest = News(
+            title=form.head.data,
+            content=form.text.data,
+            tema=form.tema.data,
+            img_news='static/img/' + filename,
+        )
+        db_sess.add(quest)
+        db_sess.commit()
+        return redirect('/quizzes')
+    que = ['sadds', 'sdsdsa', 'sdsdsd']
+    return render_template('makequizzes.html', title='Регистрация', quetions=que, form=form)
+
+
 @app.route('/addnews', methods=['GET', 'POST'])
 def addnews():
     form = NewsForm()
@@ -125,7 +190,6 @@ def addnews():
             return render_template('makenews.html', title='Регистрация',
                                    form=form,
                                    message="Пустое поле текста")
-
         if form.tema.data == '':
             return render_template('makenews.html', title='Регистрация',
                                    form=form,
@@ -153,10 +217,20 @@ def addnews():
 @login_required
 def profile():
     user = load_user(current_user.id)
-    im = user.img_user
-    print(im)
+    if user.img_user is None:
+        im = 'static/img/Noneph.jpg'
+    else:
+        im = 'static/img/' + user.img_user
     return render_template('profile.html', user=user, im=im)
 
+
+# @app.route('/Quiz/<id>')
+# def onequiz(id):
+#    print(id)
+#    db_sess = db_session.create_session()
+#    news = db_sess.query(Quest).get(id)
+#    im = quest.img_news
+#    return render_template('onenews.html', news=news, im=im)
 
 @app.route('/news/<id>')
 def onenews(id):
@@ -177,13 +251,12 @@ def profchange():
             user.name = form.name.data
         if form.email.data != '':
             user.email = form.email.data
-
         if form.about.data != '':
             user.about = form.about.data
         if form.photo.data:
             filename = photos.save(form.photo.data)
             file_url = photos.url(filename)
-            user.img_user = 'static/img/' + filename
+            user.img_user = filename
         db_sess.commit()
         return redirect('/profile')
     return render_template('changeprof.html', title='Регистрация', form=form)
