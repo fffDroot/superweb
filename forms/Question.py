2bb923caa5035aef05cf24ac69f6ae34fa940fac
Vlados <vladnaumkin2005@yandex.ru>
2022-04-16 16:40:43 +0300

    доработка создания квестов

--- forms/Question.py
+++ forms/Question.py
@@ -6,6 +6,10 @@ from wtforms.validators import DataRequired
 class QuestionForm(FlaskForm):
     head = StringField('Заголовок', validators=[DataRequired()])
     text = TextAreaField('Контент', validators=[DataRequired()])
+    text1 = StringField('1', validators=[DataRequired()])
+    text2 = StringField('2', validators=[DataRequired()])
+    text3 = StringField('3', validators=[DataRequired()])
+    text4 = StringField('4', validators=[DataRequired()])
     tema = StringField('Тема', validators=[DataRequired()])
     photo = FileField('Фото Вопроса')
     submit = SubmitField('Добавить')
