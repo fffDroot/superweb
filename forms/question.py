60a86c4ac2da475cf3aded1648b339be6706492d
Vlados <vladnaumkin2005@yandex.ru>
2022-04-17 16:43:28 +0300

    Создание квестов завершено

--- forms/question.py
+++ forms/question.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, FileField
+from wtforms import RadioField, StringField, TextAreaField, SubmitField, FileField
 from wtforms.validators import DataRequired
 
 
 class QuestionForm(FlaskForm):
     head = StringField('Заголовок', validators=[DataRequired()])
     text = TextAreaField('Контент', validators=[DataRequired()])
-    text1 = StringField('1', validators=[DataRequired()])
-    text2 = StringField('2', validators=[DataRequired()])
-    text3 = StringField('3', validators=[DataRequired()])
-    text4 = StringField('4', validators=[DataRequired()])
-    tema = StringField('Тема', validators=[DataRequired()])
+    text1 = StringField('1')
+    text2 = StringField('2')
+    text3 = StringField('3')
+    text4 = StringField('4')
+    right = StringField("Выберите правильный ответ")
     photo = FileField('Фото Вопроса')
     submit = SubmitField('Добавить')
