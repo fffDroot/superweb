5a9eee75d010d68d0f163bd34de928abcb8f4393
Ivan <gordin-vanyusha@yandex.ru>
2022-04-28 20:16:26 +0300

    make port

--- app.py
+++ app.py
@@ -523,4 +523,5 @@ def profchange():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
